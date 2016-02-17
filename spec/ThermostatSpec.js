8b429ee797331d63d973385d672bb3c634fba713
esbaddeley <esbaddeley@gmail.com>
2016-02-17 11:20:42 +0000

    Built out functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -19,11 +19,20 @@ describe ("Thermostat", function(){
     expect(thermostat.powerSaveMode).toBeTruthy();
   });
 
+  it ("is in the medium heat range on initialization", function(){
+    expect(thermostat.isMedium).toBeTruthy();
+  });
+
   describe ("up", function(){
 
     it ("raises the temperature by 1", function(){
       thermostat.up();
       expect(thermostat.temperature).toEqual(21);
+    });
+
+    it("only raises the temperature up to the maximum", function(){
+      thermostat.temperature = 25;
+      expect(function(){thermostat.up();}).toThrow('Cannot raise above 25 in power save mode');
     });
 
   });
@@ -55,6 +64,48 @@ describe ("Thermostat", function(){
       expect(thermostat.powerSaveMode).toBeTruthy();
     });
 
+
+  });
+
+  describe ("powerSaveMode on", function(){
+
+    it("can raise the temperature up to 32", function(){
+      thermostat.changePowerMode();
+      thermostat.temperature = 32;
+      expect(function(){thermostat.up();}).toThrow('Cannot raise above 32');
+    });
+
+
+  });
+
+  describe("reset", function(){
+
+    it("resets the temperature to 20", function(){
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20);
+    });
+
+  });
+
+  describe("heatRange", function(){
+
+    it("returns high if temperature is >= 25 ", function(){
+      thermostat.temperature = 30;
+      thermostat.heatRange();
+      expect(thermostat.isHigh).toBeTruthy();
+    });
+
+    it("returns medium if temperature is < 20 and >= 18 ", function(){
+      thermostat.temperature = 20;
+      thermostat.heatRange();
+      expect(thermostat.isMedium).toBeTruthy();
+    });
+
+    it("returns high if temperature is >= 25 ", function(){
+      thermostat.temperature = 16;
+      thermostat.heatRange();
+      expect(thermostat.isLow).toBeTruthy();
+    });
 
   });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,20 +1,54 @@
-function Thermostat(mode) {
+function Thermostat() {
   this.temperature = 20;
   this.powerSaveMode = true;
+  this.isHigh = false;
+  this.isMedium = true;
+  this.isLow = false;
 }
 
 Thermostat.prototype.up = function() {
+  if (this.temperature >= 25 && this.powerSaveMode) {
+      throw('Cannot raise above 25 in power save mode');
+  } else if (this.temperature >= 32) {
+      throw('Cannot raise above 32');
+  } else {
   this.temperature++;
+  this.heatRange();
+  }
 };
 
 Thermostat.prototype.down = function() {
-if (this.temperature <= 10) {
-    throw('Cannot lower below 10');
-}   else {
+  if (this.temperature <= 10) {
+      throw('Cannot lower below 10');
+  } else {
     this.temperature--;
-    }
+    this.heatRange();
+  }
 };
 
 Thermostat.prototype.changePowerMode = function() {
   this.powerSaveMode = !this.powerSaveMode;
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
+  this.heatRange();
+};
+
+Thermostat.prototype.heatRange = function(){
+  if (this.temperature >= 25) {
+      this.isHigh = true;
+      this.isMedium = false;
+      this.isLow = false;
+  } else if (this.temperature < 25 && this.temperature >=18) {
+      this.isHigh = false;
+      this.isMedium = true;
+      this.isLow = false;
+  } else if (this.temperature < 18) {
+      this.isHigh = false;
+      this.isMedium = false;
+      this.isLow = true;
+  } else {
+    throw('Thermostat broken');
+  }
 };
