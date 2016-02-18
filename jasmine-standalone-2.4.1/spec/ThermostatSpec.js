00a40220cf88f75ca6be92b8b9bf9b426b247985
Constantin <kostiantynkalinin@gmail.com>
2016-02-18 07:54:54 +0000

    User story 1

---
+++ jasmine-standalone-2.4.1/spec/ThermostatSpec.js
@@ -0,0 +1,10 @@
+'use strict'
+describe('Thermostat', function(){
+  var thermostat;
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+});
---
+++ jasmine-standalone-2.4.1/src/Thermostat.js
@@ -0,0 +1,7 @@
+'use strict';
+function Thermostat(){
+this.temperature = 20;
+}
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature
+};
