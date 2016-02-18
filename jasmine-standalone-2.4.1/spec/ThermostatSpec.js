29552e6b1ff4c75d7b85b0103e27166591e68150
Constantin <kostiantynkalinin@gmail.com>
2016-02-18 08:04:33 +0000

    User stories 2 and 3

--- jasmine-standalone-2.4.1/spec/ThermostatSpec.js
+++ jasmine-standalone-2.4.1/spec/ThermostatSpec.js
@@ -7,4 +7,12 @@ describe('Thermostat', function(){
   it('starts at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+it('increases the temperature when pushing up button', function(){
+  thermostat.increaseTemperature();
+  expect(thermost.getCurrentTemperature()).toEqual(21);
+});
+it('increases the temperature when pushing up button', function(){
+  thermostat.down();
+  expect(thermost.getCurrentTemperature()).toEqual(19);
+});
 });
--- jasmine-standalone-2.4.1/src/Thermostat.js
+++ jasmine-standalone-2.4.1/src/Thermostat.js
@@ -5,3 +5,11 @@ this.temperature = 20;
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature
 };
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
+}
+
+Thermostat.prototype.down = function() {
+  this.temperature -= 1;
+}
